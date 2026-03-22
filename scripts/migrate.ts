62409757d1e9f3b52fd65cac47b5bd8605737af2
andre-brandao <82166576+andre-brandao@users.noreply.github.com>
2026-03-22 11:55:30 -0300

    feat: migration script

---
+++ scripts/migrate.ts
@@ -0,0 +1,8 @@
+import { $ } from "bun";
+import { Resource } from "sst/resource";
+
+const CORE_PATH = `${import.meta.dir}/../packages/core`;
+
+await $`bun run db:migrate`.cwd(CORE_PATH).env({
+  DATABASE_URL: Resource.Database.url,
+});
