abaeb629c519dc211754ba9613f055d00eb55b95
andre-brandao <82166576+andre-brandao@users.noreply.github.com>
2026-05-13 13:44:58 -0300

    Update database.ts

--- infra/database.ts
+++ infra/database.ts
@@ -63,3 +63,41 @@ export const database = new sst.Linkable("Database", {
     url: $interpolate`postgresql://${role.username}:${role.password}@${role.accessHostUrl}/postgres?sslmode=require`,
   },
 })
+
+
+// const migrator_role = new planetscale.PostgresBranchRole("DatabaseMigratorRole", {
+//   database: cluster.name,
+//   organization: cluster.organization,
+//   branch: branch.name,
+//   name: `${$app.name}-migrator-${$app.stage}`,
+//   inheritedRoles: [
+//     "pg_read_all_data",
+//     "pg_write_all_data",
+//     "postgres", // Only needed for pushing schema changes
+//   ],
+
+// })
+
+
+// const migration = await command.local.run({
+//   command: "bun run db:migrate",
+//   environment: {
+//     DATABASE_URL: $interpolate`postgresql://${migrator_role.username}:${migrator_role.password}@${migrator_role.accessHostUrl}/postgres?sslmode=require`
+//   },
+//   dir: "packages/core"
+// }, {
+//   parent: migrator_role
+// })
+
+
+// const migration = new command.local.Command("DbMigration", {
+//   dir: `${process.cwd()}/packages/core`,
+//   environment: {
+//     DATABASE_URL: $interpolate`postgresql://${migrator_role.username}:${migrator_role.password}@${migrator_role.accessHostUrl}/postgres?sslmode=require`
+//   },
+
+//   create: "bun run db:migrate || ( [ $? -eq 9 ] && exit 0 ); exit $?",
+//   update: "bun run db:migrate || ( [ $? -eq 9 ] && exit 0 ); exit $?"
+// }, {
+//   dependsOn: [migrator_role]
+// })
